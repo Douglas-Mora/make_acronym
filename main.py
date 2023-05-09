cbc4799948d7ee4e8376086c88beb20734a3161c
Douglas <doug-mora@outlook.com>
2023-05-09 10:44:26 -0600

    initial solution

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Write a function which takes a string and make an acronym of it. Steps to follow:
+ - Split string to words by space char.
+ - Take every first letter from word in given string
+ - Uppercase it
+ - Join them together
+"""
+
+def make_acronym(sentence):
+    list_of_words = sentence.split(" ")
+    output = ""
+    for word in list_of_words:
+        output += word[0].capitalize()
+    return output
+
+print(make_acronym("Code Wars"))
